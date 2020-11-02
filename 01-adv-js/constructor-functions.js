c4c61d9a3c27e225af7deebad5be532be4638f7b
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-02 16:15:32 +0530

    constructor functions

---
+++ 01-adv-js/constructor-functions.js
@@ -0,0 +1,13 @@
+function Employee(id, name, salary) {
+    //this -> new object
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+
+    this.display = function () {
+        console.log(this.id, this.name, this.salary);
+    }
+}
+
+var emp = new Employee(100, 'Magesh', 10000)
+emp.display();
