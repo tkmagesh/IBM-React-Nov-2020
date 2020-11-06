f4e2d2d13bbe26adbb314db3c174c7068a5d969e
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-06 10:57:08 +0530

    middlewares in redux & logger middleware

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import productsReducer from '../reducers/productsReducer';
 import categoriesReducer from '../reducers/categoriesReducer';
@@ -8,13 +8,19 @@ const rootReducer = combineReducers({
     categories : categoriesReducer
 });
 
-//const store = createStore(productsReducer);
-//const store = createStore(categoriesReducer);
+function logger(store){
+    return function(next){
+        return function(action){
+            console.group(action.type);
+            console.log('Before -> ', store.getState());
+            console.log(action);
+            next(action);
+            console.log("After -> ", store.getState());
+            console.groupEnd();
+        }
+    }
+}
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
-//important : the following line has to REMOVED when the application is deployed for production
-window['store'] = store;
-
-// store state = { products : [], categories : []}
 export default store;
