fcb82d39bc2374ef7f74fd0e3ae2b9e27fd8d142
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-06 14:46:39 +0530

    ProductItem as a functional component

--- my-app/src/products/views/ProductItem.jsx
+++ my-app/src/products/views/ProductItem.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class ProductItem extends Component {
+/* class ProductItem extends Component {
     render() {
         const { product, toggleOutOfStock, remove } = this.props,
             { id, name, description, price, isOutOfStock, category } = product;
@@ -22,6 +22,27 @@ class ProductItem extends Component {
             </li>
         )
     }
+} */
+
+const ProductItem = ({ product, toggleOutOfStock, remove }) => {
+    const { name, description, price, isOutOfStock, category } = product;
+    return (
+        <li>
+            <span className={'name ' + (isOutOfStock ? 'outofstock' : '')}>
+                {name}
+            </span>
+            <span> {category} </span>
+            <div>{description}</div>
+            <div className="price">Rs.{price}</div>
+            <input type="checkbox"
+                checked={isOutOfStock}
+                onChange={() => toggleOutOfStock(product)}
+            />
+            <label htmlFor="">Out of Stock</label>
+            <br />
+            <input type="button" value="Remove" onClick={() => remove(product)} />
+        </li>
+    )
 }
 
 export default ProductItem;
